994e19f2fc5a8390f4410b642cc68c12b8e03cf6
dani08722 <mejias.lodan24@triana.salesianos.edu>
2026-01-08 10:40:55 +0100

    Menu añadido y switch

--- src/proyecto/Principal.java
+++ src/proyecto/Principal.java
@@ -5,6 +5,19 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int opcion;
+		do {
+			System.out.println("1. Mostrar saludo");
+			System.out.println("2. Mostrar información del grupo");
+			System.out.println("3. Mostrar despedida");
+			System.out.println("0. Salir");
+			opcion
+			
+			
+		}while(opcion!=0);
+		
+		
+		
 	}
 
 }
